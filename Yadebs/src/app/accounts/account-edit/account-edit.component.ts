28e6683417d6d6713112b82f28f4972e60e2b378
Mathias Fritsch <Mathias.Fritsch@senacor.com>
2022-10-13 16:20:16 +0200

    errorhandling

--- Yadebs/src/app/accounts/account-edit/account-edit.component.ts
+++ Yadebs/src/app/accounts/account-edit/account-edit.component.ts
@@ -4,22 +4,33 @@ import {
   FormBuilder,
   FormGroup,
   FormControl,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
 } from '@angular/forms';
 import { FloatLabelType } from '@angular/material/form-field';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-account-edit',
   templateUrl: './account-edit.component.html',
   styleUrls: ['./account-edit.component.css'],
 })
 export class AccountEditComponent implements OnInit {
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(
+    private _formBuilder: FormBuilder,
+    public dialogRef: MatDialogRef<AccountEditComponent>
+  ) {}
   accountForm = new FormGroup({
-    name: new FormControl(''),
-    age: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    number: new FormControl('', Validators.required),
   });
   ngOnInit(): void {}
 
-  setProperty(): void {
-    this.accountForm.patchValue({ name: 'some' });
+  submitForm(): void {
+    if (this.accountForm.invalid) {
+      return;
+    }
+    this.dialogRef.close();
   }
 }
